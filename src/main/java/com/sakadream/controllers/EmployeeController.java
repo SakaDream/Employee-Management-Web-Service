5a24becc41211d210be67331e0dcba343bd0bc0e
SakaDream <bahaimt@gmail.com>
2018-01-01 22:55:07 +0700

    Fix No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- src/main/java/com/sakadream/controllers/EmployeeController.java
+++ src/main/java/com/sakadream/controllers/EmployeeController.java
@@ -14,6 +14,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 /**
  * Created by Phan Ba Hai on 25/08/2017.
  */
+@CrossOrigin(origins = "*")
 @RestController
 public class EmployeeController {
     @Autowired
@@ -25,7 +26,7 @@ public class EmployeeController {
         return (employees.isEmpty()) ? new ResponseEntity<List<Employee>>(NO_CONTENT) : new ResponseEntity<List<Employee>>(employees, OK);
     }
 
-    @RequestMapping(value = "/employee", params = "id", method = GET)
+    @RequestMapping(value = "/employees", params = "id", method = GET)
     public ResponseEntity<Employee> getById(@RequestParam int id) {
         Employee employee = service.findById(id);
         return (employee == null) ? new ResponseEntity<Employee>(NOT_FOUND) : new ResponseEntity<Employee>(employee, OK);
@@ -44,14 +45,14 @@ public class EmployeeController {
         return (service.addEmployee(employee)) ? new ResponseEntity<Void>(CREATED) : new ResponseEntity<Void>(BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/updateEmployee", method = POST)
+    @RequestMapping(value = "/updateEmployee", method = PUT)
     public ResponseEntity<Void> updateEmployee(@RequestParam("id") int id, @RequestParam("token") String token, @RequestBody Employee employee) {
         if(!token.equals(System.getenv("APP_TOKEN")))
             return new ResponseEntity<Void>(BAD_REQUEST);
         return (service.updateEmployee(id, employee)) ? new ResponseEntity<Void>(OK) : new ResponseEntity<Void>(NOT_FOUND);
     }
 
-    @RequestMapping(value = "/deleteEmployee", method = POST)
+    @RequestMapping(value = "/deleteEmployee", method = DELETE)
     public ResponseEntity<Void> deleteEmployee(@RequestParam("id") int id, @RequestParam("token") String token) {
         if(!token.equals(System.getenv("APP_TOKEN")))
             return new ResponseEntity<Void>(BAD_REQUEST);
