6f1c8b8bb721362ca25eb2d66bbd1e28952d3255
SakaDream <bahaimt@gmail.com>
2018-01-02 13:45:19 +0700

    Update RESTful API to standard HTTP Method

--- src/main/java/com/sakadream/controllers/EmployeeController.java
+++ src/main/java/com/sakadream/controllers/EmployeeController.java
@@ -8,53 +8,66 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import javax.ws.rs.Produces;
+
 import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 /**
  * Created by Phan Ba Hai on 25/08/2017.
  */
 @CrossOrigin(origins = "*")
+@RequestMapping("/employees")
 @RestController
 public class EmployeeController {
     @Autowired
     EmployeeService service;
 
-    @RequestMapping(value = "/employees", method = GET)
+    @RequestMapping(method = GET)
     public ResponseEntity<List<Employee>> getAllEmployees() {
         List<Employee> employees = service.getAllEmployees();
-        return (employees.isEmpty()) ? new ResponseEntity<List<Employee>>(NO_CONTENT) : new ResponseEntity<List<Employee>>(employees, OK);
+        return (employees.isEmpty()) ? new ResponseEntity<List<Employee>>(NO_CONTENT)
+                : new ResponseEntity<List<Employee>>(employees, OK);
     }
 
-    @RequestMapping(value = "/employees", params = "id", method = GET)
+    @RequestMapping(params = "id", method = GET)
     public ResponseEntity<Employee> getById(@RequestParam int id) {
         Employee employee = service.findById(id);
-        return (employee == null) ? new ResponseEntity<Employee>(NOT_FOUND) : new ResponseEntity<Employee>(employee, OK);
+        return (employee == null) ? new ResponseEntity<Employee>(NOT_FOUND)
+                : new ResponseEntity<Employee>(employee, OK);
     }
 
-    @RequestMapping(value = "/employee", params = "name", method = GET)
+    @RequestMapping(params = "name", method = GET)
     public ResponseEntity<Employee> getByName(@RequestParam String name) {
         Employee employee = service.findByName(name);
-        return (employee.getId() == 0) ? new ResponseEntity<Employee>(NOT_FOUND) : new ResponseEntity<Employee>(employee, OK);
+        return (employee.getId() == 0) ? new ResponseEntity<Employee>(NOT_FOUND)
+                : new ResponseEntity<Employee>(employee, OK);
     }
 
-    @RequestMapping(value = "/addEmployee", method = POST)
+    @RequestMapping(method = POST, consumes = { APPLICATION_FORM_URLENCODED_VALUE,
+            APPLICATION_JSON_UTF8_VALUE }, produces = { APPLICATION_ATOM_XML_VALUE, APPLICATION_JSON_UTF8_VALUE })
     public ResponseEntity<Void> addEmployee(@RequestParam("token") String token, @RequestBody Employee employee) {
-        if(!token.equals(System.getenv("APP_TOKEN")))
+        if (!token.equals(System.getenv("APP_TOKEN")))
             return new ResponseEntity<Void>(BAD_REQUEST);
-        return (service.addEmployee(employee)) ? new ResponseEntity<Void>(CREATED) : new ResponseEntity<Void>(BAD_REQUEST);
+        return (service.addEmployee(employee)) ? new ResponseEntity<Void>(CREATED)
+                : new ResponseEntity<Void>(BAD_REQUEST);
     }
 
-    @RequestMapping(value = "/updateEmployee", method = PUT)
-    public ResponseEntity<Void> updateEmployee(@RequestParam("id") int id, @RequestParam("token") String token, @RequestBody Employee employee) {
-        if(!token.equals(System.getenv("APP_TOKEN")))
+    @RequestMapping(method = PUT, consumes = { APPLICATION_FORM_URLENCODED_VALUE,
+            APPLICATION_JSON_UTF8_VALUE }, produces = { APPLICATION_ATOM_XML_VALUE, APPLICATION_JSON_UTF8_VALUE })
+    public ResponseEntity<Void> updateEmployee(@RequestParam("id") int id, @RequestParam("token") String token,
+            @RequestBody Employee employee) {
+        if (!token.equals(System.getenv("APP_TOKEN")))
             return new ResponseEntity<Void>(BAD_REQUEST);
-        return (service.updateEmployee(id, employee)) ? new ResponseEntity<Void>(OK) : new ResponseEntity<Void>(NOT_FOUND);
+        return (service.updateEmployee(id, employee)) ? new ResponseEntity<Void>(OK)
+                : new ResponseEntity<Void>(NOT_FOUND);
     }
 
-    @RequestMapping(value = "/deleteEmployee", method = DELETE)
+    @RequestMapping(method = DELETE, consumes = { APPLICATION_FORM_URLENCODED_VALUE,
+        APPLICATION_JSON_UTF8_VALUE }, produces = { APPLICATION_ATOM_XML_VALUE, APPLICATION_JSON_UTF8_VALUE })
     public ResponseEntity<Void> deleteEmployee(@RequestParam("id") int id, @RequestParam("token") String token) {
-        if(!token.equals(System.getenv("APP_TOKEN")))
+        if (!token.equals(System.getenv("APP_TOKEN")))
             return new ResponseEntity<Void>(BAD_REQUEST);
         return (service.deleteEmployee(id)) ? new ResponseEntity<Void>(OK) : new ResponseEntity<Void>(NOT_FOUND);
     }
